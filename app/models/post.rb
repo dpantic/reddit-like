76de8a450b6d8424fff69517d77db7bb88ef5ce1
Dejan <dpantic@gmail.com>
2012-02-16 23:34:14 +0100

    Reddit like app for the Homework_2.

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ActiveRecord::Base
+  belongs_to :user
+  
+  validates :comment, :length => { :maximum => 10 }
+  #validates_http_url :url, :wrong_response => "did not return a successful HTTP code"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedditLike::Application.config.secret_token = '9406af1cf716b43ecb57eee51f89385968871ee15f2d3e9cdddc734d755dc4dc5958ad46f3ad74b2fcea2c9f26a56d1ae15affae5b8e3dd1a9db8576aaa7e484'
