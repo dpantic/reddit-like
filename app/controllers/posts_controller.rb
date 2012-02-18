ed336e326dc779d2500453e406998c9b39a0652d
Dejan <dpantic@gmail.com>
2012-02-18 20:41:38 +0100

    Final Homework_2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
   # GET /posts/1.xml
   def show
     @post = Post.find(params[:id])
-
+    @users = User.all
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @post }
